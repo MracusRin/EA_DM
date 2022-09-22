6ec62bfda3c3ed2977dcede2ad8e4813914b5571
MracusRin <aleksandrovi44@gmail.com>
2022-09-22 13:33:38 +0500

    Init commit

---
+++ src/main/java/ru/em_da/EmDaApplication.java
@@ -0,0 +1,13 @@
+package ru.em_da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmDaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/em_da/EmDaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.em_da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
